c5e69d5da6df0ddf4defbf5b6c64d4bcf9c33bc6
FreimanRuiz <freiman@bewe.io>
2024-11-14 01:46:52 -0500

    fix new views styles

--- src/components/ProjectsArea/SingleProject.js
+++ src/components/ProjectsArea/SingleProject.js
@@ -1,40 +1,60 @@
 import React from "react";
 import { Image } from "react-bootstrap";
+import {
+  calcularPorcentaje,
+  formatPrice,
+  calcularDiasConMoment,
+} from "@/utils/helpers";
 import Link from "../Reuseable/Link";
 
 const SingleProject = ({ project = {} }) => {
-  const { image, category, date, title, goal, raised } = project;
+  const { id, image, category, launchDate, title, campaignDuration, fundingAmount } = project;
 
   return (
     <div className="explore-projects-item mt-30">
       <div className="explore-projects-thumb">
-        <Image src={image.src} alt="explore-projects" />
-        <a href="#">
+        <Image src={image} alt="explore-projects" />
+        {/* <a href="#">
           <i className="fa fa-heart"></i>
-        </a>
+        </a> */}
       </div>
       <div className="explore-projects-content">
         <div className="item d-flex align-items-center">
           <span>{category}</span>
           <p>
-            <i className="fa fa-clock-o"></i> {date}
+            <i className="fa fa-clock-o"></i> {calcularDiasConMoment(
+                      launchDate,
+                      campaignDuration
+                    )} días restantes
           </p>
         </div>
-        <Link href="/single-project">
+        <Link href={`/single-project/${id}`}>
           <h3 className="title">{title}</h3>
         </Link>
         <div className="projects-range">
           <div className="projects-range-content">
             <ul>
-              <li>Raised:</li>
-              <li>{raised}%</li>
+              <li>Recaudado:</li>
+              <li>{calcularPorcentaje(fundingAmount, fundingAmount)}%</li>
             </ul>
-            <div className="range"></div>
+            <div className="progress">
+              <div
+                className="progress-bar"
+                role="progressbar"
+                style={{
+                  width: `${calcularPorcentaje(fundingAmount, fundingAmount)}%`,
+                }}
+                aria-valuenow={calcularPorcentaje(fundingAmount, fundingAmount)}
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+              </div>
+            </div>
           </div>
         </div>
         <div className="projects-goal">
           <span>
-            Goal: <span>{goal} USD</span>
+            Meta: <span>{formatPrice(fundingAmount)} COP</span>
           </span>
         </div>
       </div>
